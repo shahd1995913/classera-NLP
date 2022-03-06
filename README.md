b7944a6ab81b87eb866eb80d259a88e140efd354
shahd1995913 <shahdalkhatib95@gmail.com>
2022-03-06 23:32:49 +0300

    last update 2022-3-6

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # classera-NLP
+
+# To be In loacl Host
+
+### nohup tensorflow_model_server \
+### --rest_api_port=8501 \
+### --model_name=tflearn \
+### --model_base_path="/home/shahd/classera-NLP" >server.log 2>&1
--- chatbot/chatbotpy.py
+++ chatbot/chatbotpy.py
@@ -90,27 +90,24 @@ train_y = list(training[:, 1])
 
 # ! pip install tflearn 
 
-
 import tflearn
 # tf.reset_default_graph()
 tf.compat.v1.reset_default_graph()
 # Build neural network
 net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net,32)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 
 # Define model and setup tensorboard
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 # Start training (apply gradient descent algorithm)
-model.fit(train_x, train_y, n_epoch=500, batch_size=10, show_metric=True)
+model.fit(train_x, train_y, n_epoch=1, batch_size=10, show_metric=True)
 model.save('model.tflearn')
-
-# essay greading -- text  NLP 
-
+ 
+#essay greading -- text  NLP 
 ERROR_THRESHOLD = 0.25
-
 def clean_up_sentence(sentence):
     # tokenize the pattern
     sentence_words = nltk.word_tokenize(sentence)
